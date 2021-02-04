f4c6b1761df5ef78eb5d6bdd5e3dcde85ba5b60a
WilmerRN <wilmer-endo@hotmail.com>
2021-02-03 23:03:45 -0600

    otro commit

--- Myproject_site/polls/views.py
+++ Myproject_site/polls/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index")
     #esto es una vista
+#detalles de la encuesta
+def detail(request, question_id):
+    return HttpResponse("You're looking at question."% question_id)
+#resultados de la encuesta
+def results(request, question_id):
+    response = "You're looking at the results of question%s"
+    return HttpResponse(response % question_id)
+#para contar las votaciones de la encuesta
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
